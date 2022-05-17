374fc37181aa7c228d99e793184d3f08a3ff6020
Agirona <agirona@student.42lyon.fr>
2022-05-17 20:16:19 +0200

    Add some member function for Channel Class

--- Channel.hpp
+++ Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: agirona <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 19:57:13 by agirona           #+#    #+#             */
-/*   Updated: 2022/05/13 20:36:54 by agirona          ###   ########lyon.fr   */
+/*   Updated: 2022/05/17 20:15:51 by agirona          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,26 @@
 # define	Channel_HPP
 
 # include <iostream>
+# include <list>
+# include "Client.hpp"
 
 class	Channel
 {
 	private :
+		std::list<Client>	_user;
+		std::list<Client>	_op;
 
 	public :
-		Channel();
+		Channel(const Client &client);
 		~Channel();
+
+		int		addUser(const Client &client);
+		int		deleteUser(Client &client);
+		int		addOperator(const Client &client);
+		int		deleteOperator(Client &client);
+		std::list<Client>	getOp(void) const;
+		std::list<Client>	getUser(void) const;
+		std::list<Client>	getAllUser(void);
 };
 
 #endif
